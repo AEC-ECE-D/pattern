6e27bf94ae3aa2114da605575fd0874f02288b71
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-01-26 12:19:03 +0530

    Add files via upload

---
+++ pattern11.py
@@ -0,0 +1,14 @@
+n=int(input("enter no of rows"))
+for i in range(n):
+    print('*'*(i+1))
+"""
+enter no of rows7
+*
+**
+***
+****
+*****
+******
+*******
+>>>
+"""
---
+++ pattern12.py
@@ -0,0 +1,20 @@
+rows = int(input())
+
+for i in range(rows,0,-1):
+
+    num = i
+
+    for j in range(1,i+1):
+
+        print(j, end="")
+
+    print("\r")
+"""
+6
+123456
+12345
+1234
+123
+12
+1
+"""
